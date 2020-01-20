61f1b4b8051e416ff9feddc3ba91eca6f9d86f2f
carlcnx <carlcnx@outlook.com>
2020-01-20 15:29:00 +0100

    Add images and publish data

--- NetworkCamera.Wpf/Properties/Resources.Designer.cs
+++ NetworkCamera.Wpf/Properties/Resources.Designer.cs
@@ -243,6 +243,16 @@ namespace NetworkCamera.Wpf.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap NetcamBlue1080 {
+            get {
+                object obj = ResourceManager.GetObject("NetcamBlue1080", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap NetcamWhite {
             get {
                 object obj = ResourceManager.GetObject("NetcamWhite", resourceCulture);
