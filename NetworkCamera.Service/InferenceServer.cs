2d1c426e96f37a7eccae47cadea792d1b08fa8f3
carlcn <carlcn@axis.com>
2020-10-30 10:07:49 +0100

    Add reference to Bitmap handling

--- NetworkCamera.Service/InferenceServer.cs
+++ NetworkCamera.Service/InferenceServer.cs
@@ -12,13 +12,78 @@
  * limitations under the License.
  */
 
+using Google.Protobuf;
+using Grpc.Core;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Tensorflow;
+using Tensorflow.Serving;
+using static Tensorflow.Serving.PredictionService;
+
 namespace NetworkCamera.Service
 {
     public class InferenceServer
     {
-        public void Startup(string inferenceServer)
+        public unsafe void Startup(string inferenceServer, string certificate = default)
         {
-            if (string.IsNullOrEmpty(inferenceServer)) return;
+            if (string.IsNullOrEmpty(inferenceServer)) throw new ArgumentNullException(nameof(inferenceServer));
+
+            ChannelCredentials channelCredentials = 
+                certificate == default ? ChannelCredentials.Insecure : new SslCredentials(certificate);
+            var channel = new Channel(inferenceServer, channelCredentials);
+            channel.ConnectAsync(DateTime.Now.AddSeconds(10).ToUniversalTime()).Wait();
+            var client = new PredictionServiceClient(channel);
+
+            // Set image
+            const string imageFile = @"TestData/grace_hopper_300x300.bmp";
+            const string model = @"testdata/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite";
+
+            var bmp = new Bitmap(imageFile);
+            int channels = 3;
+            int width = bmp.Width;
+            int height = bmp.Height;
+            int size = channels * width * height;
+            BitmapData bmpData = bmp.LockBits(
+                new Rectangle(0, 0, width, height),
+                ImageLockMode.WriteOnly,
+                bmp.PixelFormat);
+
+            ReadOnlySpan<byte> byteSpan = new ReadOnlySpan<byte>(bmpData.Scan0.ToPointer(), size);
+            ByteString image = ByteString.CopyFrom(byteSpan);
+
+            //Unlock the pixels
+            bmp.UnlockBits(bmpData);
+
+            var shape = new TensorShapeProto
+            {
+                Dim = { new []
+                {
+                    new TensorShapeProto.Types.Dim { Name = "", Size = 1 },
+                    new TensorShapeProto.Types.Dim { Name = nameof(height), Size = height },
+                    new TensorShapeProto.Types.Dim { Name = nameof(width), Size = width },
+                    new TensorShapeProto.Types.Dim { Name = nameof(channels), Size = channels },
+                } }
+            };
+
+            var proto = new TensorProto
+            {
+                TensorShape = shape,
+                Dtype = DataType.DtUint8,
+                TensorContent = image
+            };
+
+            var request = new PredictRequest
+            {
+                ModelSpec = new ModelSpec { Name = model }
+            };
+            request.Inputs.Add("data", proto);
+            PredictResponse response = client.Predict(request);
+
+
+
+            //var a = Tensorflow.TensorProto.
+
         }
     }
 }
